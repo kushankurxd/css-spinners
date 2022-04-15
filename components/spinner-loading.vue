7c935783eb05751fc73eb70f863598d91565c67e
kushankurdas <kushankur@gomigo.io>
2022-04-15 10:47:40 +0530

    spinner-2 added

---
+++ components/spinner-loading.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="spinner-loading-wrapper">
+    <div class="spinner-loading">
+      <div class="spinner-loading spinner-loading-linear"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SpinnerLoadingComponent",
+};
+</script>
+
+<style scoped>
+.spinner-loading-wrapper {
+  width: 140px;
+  height: 140px;
+  margin-left: 200px;
+}
+
+.spinner-loading {
+  box-sizing: border-box;
+  width: 100%;
+  height: 100%;
+  border: 10px solid transparent;
+  border-top-color: #4bc8eb;
+  border-bottom-color: #f13a8f;
+  border-radius: 50%;
+  animation: rotate 5s linear infinite;
+}
+
+.spinner-loading-linear {
+  border-top-color: #36f372;
+  border-bottom-color: #fff;
+  animation-duration: 2.5s;
+}
+
+@keyframes rotate {
+  0% {
+    transform: scale(1) rotate(360deg);
+  }
+  50% {
+    transform: scale(0.8) rotate(-360deg);
+  }
+  100% {
+    transform: scale(1) rotate(360deg);
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,19 @@
 <template>
   <div class="container">
     <ZoomLoadingComponent />
+    <SpinnerLoadingComponent />
   </div>
 </template>
 
 <script>
 import ZoomLoadingComponent from "../components/zoom-loading.vue";
+import SpinnerLoadingComponent from "../components/spinner-loading.vue";
 
 export default {
   name: "IndexPage",
   components: {
     ZoomLoadingComponent,
+    SpinnerLoadingComponent,
   },
 };
 </script>
@@ -19,7 +22,7 @@ export default {
 .container {
   width: 100vw;
   height: 100vh;
-  background-color: #000;
+  background-color: #180a0a;
   display: flex;
   justify-content: center;
   align-items: center;

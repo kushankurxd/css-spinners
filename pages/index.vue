df5687c0fa318fbaed6ebe8a8fcc001ac3857f0b
kushankurdas <kushankur@gomigo.io>
2022-04-15 10:42:05 +0530

    spinner-1 added

---
+++ pages/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="container">
+    <ZoomLoadingComponent />
+  </div>
+</template>
+
+<script>
+import ZoomLoadingComponent from "../components/zoom-loading.vue";
+
+export default {
+  name: "IndexPage",
+  components: {
+    ZoomLoadingComponent,
+  },
+};
+</script>
+
+<style scoped>
+.container {
+  width: 100vw;
+  height: 100vh;
+  background-color: #000;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
